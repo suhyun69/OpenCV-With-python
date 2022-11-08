de681e5745f391d7bfcf86ecf89713c7971c6788
suhpark <suhpark@gmarket.com>
2022-11-08 23:34:51 +0900

    2.1 : 영상 파일 읽기 및 화면표시

---
+++ chapter2/0201.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+imageFile = '../data/lena.jpg'
+img =cv2.imread(imageFile) # cv2.IMREAD_COLOR
+img2 = cv2.imread(imageFile, 0) # cv2.IMREAD_GRAYSCALE
+
+cv2.imshow('Lena color', img)
+cv2.imshow('Lena grayscale', img2)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
