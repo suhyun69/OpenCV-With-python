75daeaa01ebdb8d34172454428d4ab4e747c7027
suhpark <suhpark@gmarket.com>
2022-11-10 16:14:47 +0900

    3.2 : 직선-사각형 교차점

---
+++ chapter3/0302.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+img = np.zeros(shape=(512, 512, 3), dtype=np.uint8) + 255
+
+x1, x2 = 100, 400
+y1, y2 = 100, 400
+cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255))
+
+pt1 = 120, 50
+pt2 = 300, 500
+cv2.line(img, pt1, pt2, (255, 0, 0), 2)
+
+imgRect = (x1, y1, x2-x1, y2-y1)
+retval, rpt1, rpt2 = cv2.clipLine(imgRect, pt1, pt2)
+# 좌표 pt1, pt2에 의한 직선과 imgRect에 정의된 사각형의 교차좌표를 계산한다
+# 직선이 사각형과 만나면 retval=True이고, 교차점은 rpt1과 rpt2에 계산한다.
+# 직선과 사각형이 만나지 않으면 반환 값은 retVal=False이다
+if retval:
+    cv2.circle(img, rpt1, radius=5, color=(0, 255, 0), thickness=-1)
+    cv2.circle(img, rpt2, radius=5, color=(0, 255, 0), thickness=-1)
+
+cv2.imshow('img', img)
+cv2.waitKey()
+cv2.destroyAllWindows()
